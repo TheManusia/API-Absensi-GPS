76a75c712a9c343badeec22e9f08e2a0df027f0a
AudiantoFari <minecraftindo0@gmail.com>
2020-04-02 22:36:29 +0700

    Memperbaiki proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -15,8 +15,10 @@ die(json_encode($response));
 }
 
 $query = mysqli_query($con, "SELECT * FROM user WHERE username='$username' AND password=md5('$password')");
+$query1 = mysqli_query($con, "SELECT * FROM admin WHERE username='$username' AND password=md5('$password')");
 
 $row = mysqli_fetch_array($query, MYSQLI_ASSOC);
+$row1 = mysqli_fetch_array($query1, MYSQLI_ASSOC);
 
 if (!empty($row)){
 $response = new usr();
@@ -25,15 +27,23 @@ $response->message = "Selamat datang ".$row['nama'];
 $response->id = $row['username'];
 $response->username = $row['nama'];
 $response->gambar = $row['gambar'];
+$response->role = "User";
 die(json_encode($response));
+
+} elseif (!empty($row1)) {
+    $response = new usr();
+    $response->success = 1;
+    $response->message = "Selamat datang " . $row['nama'];
+    $response->id = $row['username'];
+    $response->username = $row['nama'];
+    $response->role = "Admin";
+    die(json_encode($response));
 
 } else {
 $response = new usr();
-$response->success = 0;
+$response->success = 0; 
 $response->message = "Username atau password salah";
 die(json_encode($response));
 }
 
 mysqli_close($con);
-
-?>
